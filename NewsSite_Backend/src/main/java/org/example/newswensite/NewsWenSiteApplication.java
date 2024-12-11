9580e974d0bb56fed35ca877a137b785cfa726bd
alirezaferdosi <mralirezaferdosi@gmail.com>
2024-12-11 23:00:24 +0330

    first commit

---
+++ NewsSite_Backend/src/main/java/org/example/newswensite/NewsWenSiteApplication.java
@@ -0,0 +1,12 @@
+package org.example.newswensite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWenSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsWenSiteApplication.class, args);
+    }
+}
