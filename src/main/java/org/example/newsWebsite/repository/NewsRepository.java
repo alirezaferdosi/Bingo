b73f185bff7edc53dac6e414a51814595f43f671
alirezaferdosi <mralirezaferdosi@gmail.com>
2024-12-15 11:46:47 +0330

    firt commit

--- src/main/java/org/example/newsWebsite/repository/NewsRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.newsWebsite.repository;
-
-import org.example.newsWebsite.model.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
